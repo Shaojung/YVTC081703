00b3436a181b43a3f10f61265e98ba277d5318a4
Shaojung <shaojung@gmail.com>
2017-08-17 13:30:33 +0800

    getExternalFilesDir

--- app/src/main/java/com/example/yvtc/yvtc081703/MainActivity.java
+++ app/src/main/java/com/example/yvtc/yvtc081703/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.yvtc.yvtc081703;
 
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import java.io.File;
@@ -31,6 +33,19 @@ public class MainActivity extends AppCompatActivity {
     public void click2(View v)
     {
         File f = new File(getExternalFilesDir("Data") + File.separator + "test2.txt");
+        try {
+            FileWriter fw = new FileWriter(f);
+            fw.write("Hello World");
+            fw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public void click3(View v)
+    {
+        File exf = Environment.getExternalStorageDirectory();
+        Log.d("EFILE", exf.getAbsolutePath());
+        File f = new File(exf.getAbsolutePath() + File.separator + "test3.txt");
         try {
             FileWriter fw = new FileWriter(f);
             fw.write("Hello World");
