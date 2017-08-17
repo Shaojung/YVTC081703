92257b4a822dd2a9bbec71ce7a89630d7c14a6c9
Shaojung <shaojung@gmail.com>
2017-08-17 11:33:34 +0800

    Initial commit

---
+++ app/src/main/java/com/example/yvtc/yvtc081703/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.yvtc.yvtc081703;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void click1(View v)
+    {
+        File f = new File(getFilesDir().getAbsolutePath() + File.separator + "test1.txt");
+        try {
+            FileWriter fw = new FileWriter(f);
+            fw.write("Hello World");
+            fw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
